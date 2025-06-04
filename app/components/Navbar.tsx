ca61dacc24a34050dfdb287be9f44d4f9d7f5ca5
Chibundu <ndubest56@gmail.com>
2025-06-04 17:39:41 +0100

    navbar section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,33 @@
+import { auth, signOut } from '@/auth'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-export default function Navbar() {
-  return <header>Navbar</header>
+export default async function Navbar() {
+  const session = await auth()
+  return (
+    <header className='bg-white shadow-md px-5 py-3'>
+      <nav className='flex justify-between items-center'>
+        <Link href='/'>
+          <Image src='/logo.png' alt='Logo' width={50} height={50} />
+        </Link>
+
+        <div className='flex items-center gap-5'>
+          {session && session?.user ? (
+            <>
+              <Link href='/startup/create'>
+                <span>Create</span>
+              </Link>
+              <button onClick={signOut}>
+                <span>Logout</span>
+              </button>
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : null}
+        </div>
+      </nav>
+    </header>
+  )
 }
